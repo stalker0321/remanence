e3f82bc08aef46538583134e62aefb48c5a5b44a
Mykola Hryshyn <74495695+stalker0321@users.noreply.github.com>
2026-08-23 03:51:25 +0000

    build: align Compose dependencies with API 36

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     // AGP 9 includes Kotlin for Android modules. Kotlin JVM is only for :core:model.
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }

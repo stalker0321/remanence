715f6ee6bd4f164d01e70b5096d0ad34f93357c6
Mykola Hryshyn <74495695+stalker0321@users.noreply.github.com>
2026-08-23 03:47:58 +0000

    build: add core data module

---
+++ android/core/data/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = "postmark.core.data"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    buildToolsVersion = libs.versions.buildTools.get()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.toVersion(libs.versions.jdk.get())
+        targetCompatibility = JavaVersion.toVersion(libs.versions.jdk.get())
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
---
+++ android/core/data/src/test/kotlin/postmark/core/data/ModuleSmokeTest.kt
@@ -0,0 +1,11 @@
+package postmark.core.data
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ModuleSmokeTest {
+    @Test
+    fun testRunnerIsWired() {
+        assertEquals(2, 1 + 1)
+    }
+}
